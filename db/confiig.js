698d4bf2b07ecedd343aba2c128aca9e3643d85e
AbubakarCinnova <89609003+AbubakarChaudhary@users.noreply.github.com>
2023-12-12 01:06:40 +0500

    Update confiig.js

--- db/confiig.js
+++ db/confiig.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose
   .connect("mongodb+srv://spark:HBSzTzdc1HXtzfQ8@cluster0.q8tzcgf.mongodb.net/")
   .then(() => {
-    console.log("Connected to MongoDB Atlas!");
+    console.log("Connected to MongoDB Atlas!!");
   })
   .catch((err) => {
     console.error("Error connecting to MongoDB Atlas:", err);
