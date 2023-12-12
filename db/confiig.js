d7c12254310c3895ce075b6209fc29cdd54ed27c
AbubakarCinnova <89609003+AbubakarChaudhary@users.noreply.github.com>
2023-12-12 16:03:47 +0500

    Update confiig.js

--- db/confiig.js
+++ db/confiig.js
@@ -5,7 +5,7 @@ mongoose
     "mongodb+srv://spark:HBSzTzdc1HXtzfQ8@cluster0.q8tzcgf.mongodb.net/e-commerce"
   )
   .then(() => {
-    console.log("Connected to MongoDB Atlas!!");
+    console.log("Connected to e-commerce database on MongoDB Atlas!!");
   })
   .catch((err) => {
     console.error("Error connecting to MongoDB Atlas:", err);
